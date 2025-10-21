50683cb86ebd84dad7d7b56863fb7e925d0f8cf7
livmkhanaws <livmkhanaws@gmail.com>
2025-10-21 14:17:42 +0500

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,1 +1,9 @@
-
+# app.py
+# this is a test commit 
+def add(a, b):
+    return a + b 
+# testing addition functionality
+def test_add ():
+    assert add(1, 2) == 3
+    assert add(1, -1) == 0
+  
